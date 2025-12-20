b79855e695281ce678dd25cc75cfcdf2b1053969
eloisesouhaut <esouhaut@gmail.com>
2025-12-20 18:15:40 +0100

    Update Projet_BTC_2_groupe_39.py

--- Projet_BTC_2_groupe_39.py
+++ Projet_BTC_2_groupe_39.py
@@ -9,36 +9,35 @@ Created on Sun Nov 30 14:28:40 2025
 
 from matplotlib import pyplot as plt
 import math 
-
+#Ask user for CSV file name
 filepath = input("Enter the CSV name file (without extention) :")
-filepath = [filepath,'csv']
-filepath = '.'.join(filepath)
-   
+filepath = filepath + ".csv"
+# Retrieve all mouse IDs
 mouse_list = []
 
 fd = open(filepath, "r")
 
-#skip 1st line
+#skip header
 line = fd.readline()
-#retrieve 1st data line (2nd line)
 line = fd.readline()
 
 while line != "":
     line = line.replace("\n", "")
     data = line.split(";")
-    
-    mouse_id = data[4]
-    if mouse_id not in mouse_list:
-        mouse_list.append(mouse_id)
-
-
+   
+   if len(data) == 11:
+        mouse_id = data[4]
+        if mouse_id not in mouse_list:
+            mouse_list.append(mouse_id)
+           
     line = fd.readline()
 
 fd.close()
+print ("Number of mice detected :", len(mouse_list))
 
+# Plot fecal data for each mouse
 
-
-figure, axis = plt.subplots()
+figure, axes = plt.subplots()
 
 for mouse_id in mouse_list : 
 
@@ -46,28 +45,30 @@ for mouse_id in mouse_list :
     y=[]
     fd = open(filepath, "r")
 
-    #skip 1st line
+    #skip header
     line = fd.readline()
-    #retrieve 1st data line (2nd line)
     line = fd.readline()
  
     while line != "":
         line = line.replace("\n", "")
         data = line.split(";")
+       
         if data[4]==mouse_id :
             if data[2] == 'fecal':
                 x_value = float(data[7])
                 y_value = math.log10(float(data[8]))
+               
                 x.append(x_value)
                 y.append(y_value)
+
+            
                 #get color for treatment 
                 treatment= data[5]
-
-
 
         line = fd.readline()
     
     fd.close()
+   #Choose color depending on treatment
     clr = 'blue'
     if treatment == 'ABX':
         clr = 'green'
@@ -76,7 +77,9 @@ for mouse_id in mouse_list :
 
 
 # draw curve    
-    axis.plot(x,y,label = mouse_id,color = clr,alpha=0.5)
+    axes.plot(x,y,label = mouse_id,color = clr,alpha=0.5)
+#Graph formatting 
 figure.savefig("out.png", dpi=200)
 figure.legend(loc='right')        
     
+
